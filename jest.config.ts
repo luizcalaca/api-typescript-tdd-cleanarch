ef5a66c1d8e741b832ac356dac1e5e6023cb9729
Luiz Calaça <luizcalaca@gmail.com>
2022-07-19 22:55:50 -0300

    chore: add coverage jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ export default {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   testEnvironment: 'node',
+  collectCoverage: true,
   coverageDirectory: 'coverage',
   transform: {
     '.+\\.ts$': 'ts-jest'
